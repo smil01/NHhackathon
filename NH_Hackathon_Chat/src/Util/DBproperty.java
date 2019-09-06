c5a94c537dae3916dee13c3e51ccd5333ce84ddb
user <user@121.166.68.154>
2019-09-06 18:15:58 +0900

    AWS - RDS 적용 커밋

--- NH_Hackathon_Chat/src/Util/DBproperty.java
+++ NH_Hackathon_Chat/src/Util/DBproperty.java
@@ -3,7 +3,7 @@ package Util;
 
 public class DBproperty {
 	static public String Class = "oracle.jdbc.driver.OracleDriver";
-	static public String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
-	static public String user = "hr";
-	static public String password = "hr";
+	static public String url = "jdbc:oracle:thin:@oracle11gr2.cwy0udnhq7ul.ap-northeast-2.rds.amazonaws.com:1521:ORCL";
+	static public String user = "smil01";
+	static public String password = "dh920302";
 }
